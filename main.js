674e21f2c6bf1f9200144d0b0dac70926ea5c596
wlissis <souzawlissis@gmail.com>
2023-02-24 14:24:34 -0300

    validação

--- main.js
+++ main.js
@@ -16,31 +16,92 @@ const inputCvc = document.querySelector(".cvc.info input");
 const formCard = document.querySelector("form");
 const successCard = document.querySelector(".success");
 
-console.log(formCard)
+const fields = document.querySelectorAll("[required]")
 
-inputName.addEventListener("input",(e)=>{
-    cardName.innerText = e.target.value
-});
+// console.log(fields);
 
-inputMes.addEventListener("input",(e)=>{
-    mesDate.innerText = e.target.value
-});
+function validateField(field) {
+    // logica para verificar erros
+    function verifyErrors() {
+        let foundError = false;
 
-inputAno.addEventListener("input",(e)=>{
-    anoDate.innerText = e.target.value
-});
+        for (const error in field.validity) {
+            // se nao for custom error
+            //entao verifica se tem error
+            if (field.validity[error] && !field.validity.valid) {
+                foundError = error
+            }
+        }
 
-inputNumber.addEventListener("input",(e)=>{
-    numberCard.innerText = e.target.value
-});
+        return foundError;
+    }
 
-inputCvc.addEventListener("input",(e)=>{
-    cvcCard.innerText = e.target.value
-});
+    function setCustomMessage(message) {
+        const spanError = field.parentNode.querySelector("span.error");
 
-formCard.addEventListener("submit",(e)=>{
+        if (message) {
+            spanError.classList.add("active");
+            spanError.innerHTML = message;
+            field.style.border = "1px solid red"
+        }
+        if (message == "") {
+            spanError.style.display = "none";
+            spanError.innerHTML = "";
+        }
+    }
+    return function () {
+        if (verifyErrors()) {
+            setCustomMessage("campo obrigatorio");
+        } else {
+            setCustomMessage("")
+        }
+
+    };
+}
+
+function customValidation(event) {
+    const field = event.target
+
+    // essa var recebe ua funcao como retorno 
+    const validation = validateField(field);
+
+    //executando a funcao
+    validation();
+}
+
+for (const field of fields) {
+    field.addEventListener("invalid", (event) => {
+        //eliminar o bubble que aparece
+        event.preventDefault();
+        customValidation(event)
+    });
+    field.addEventListener("click", () => field.style.border = "1px solid purple");
+    field.addEventListener("blur", () => {
+        customValidation
+        field.style.border = "1px solid silver"
+    });
+}
+
+
+
+function getInputValue(event, campo) {
+
+    campo.innerText = event.target.value
+}
+
+inputName.addEventListener("input", (event) => getInputValue(event, cardName));
+
+inputMes.addEventListener("input", (event) => getInputValue(event, mesDate));
+
+inputAno.addEventListener("input", (event) => getInputValue(event, anoDate))
+
+inputNumber.addEventListener("input", (event) => getInputValue(event, numberCard))
+
+inputCvc.addEventListener("input", (event) => getInputValue(event, cvcCard))
+
+formCard.addEventListener("submit", (e) => {
     e.preventDefault();
-    formCard.style.display="none"
-    successCard.style.display="flex"
+    formCard.style.display = "none"
+    successCard.style.display = "flex"
     console.log(inputAno.value)
 })
